eae8635997ee8dfb27fa8c9c908f85be82513292
Vinayak Mahavar <vinayakmahavar45@gmail.com>
2026-03-08 15:14:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1013588681-626a8",
+  "appId": "1:546955373171:web:02f6ae803a1711cc565806",
+  "apiKey": "AIzaSyDT44mvBh3KTDkmeftvWLv_kMUkgs8LQng",
+  "authDomain": "studio-1013588681-626a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "546955373171"
+};
